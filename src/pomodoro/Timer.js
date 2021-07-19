abdf2383b4afdb0d19df66eea54ace8e97e37f08
Kittaka <jdavisson87@gmail.com>
2021-07-19 11:18:57 -0500

    adjusted timer component to follow given layout

--- src/pomodoro/Timer.js
+++ src/pomodoro/Timer.js
@@ -11,17 +11,14 @@ const Timer = ({
     <div className="row mb-2">
       <div className="col">
         {/* TODO: Update message below to include current session (Focusing or On Break) total duration */}
-        {timeRunning ? (
-          <h2 data-testid="session-title">
-            {label} for {formatTime(timeDuration)} minutes
-          </h2>
-        ) : (
-          <h2>Paused</h2>
-        )}
+        <h2 data-testid="session-title">
+          {label} for {formatTime(timeDuration)} minutes
+        </h2>
         {/* TODO: Update message below correctly format the time remaining in the current session */}
         <p className="lead" data-testid="session-sub-title">
           {formatTime(timeRemaining)} remaining
         </p>
+        {!timeRunning && <h2>Paused</h2>}
       </div>
     </div>
   );
