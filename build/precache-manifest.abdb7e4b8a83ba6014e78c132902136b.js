03a6d055e561f0d2caee822391ea0772c1e980cc
Kittaka <jdavisson87@gmail.com>
2021-07-19 11:33:52 -0500

    trying to create git page

---
+++ build/precache-manifest.abdb7e4b8a83ba6014e78c132902136b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657fa1e0fd2d6fe72497ef579db5b515",
+    "url": "/thinkful-pomodoro/index.html"
+  },
+  {
+    "revision": "aa7e92496f847c3d4fa6",
+    "url": "/thinkful-pomodoro/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/thinkful-pomodoro/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/thinkful-pomodoro/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa7e92496f847c3d4fa6",
+    "url": "/thinkful-pomodoro/static/js/main.1d044235.chunk.js"
+  },
+  {
+    "revision": "44b0cc2c969902a566d6",
+    "url": "/thinkful-pomodoro/static/js/runtime-main.68054dde.js"
+  }
+]);
